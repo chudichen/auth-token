a50a10f191f505a99c84d05175e5a132c4180938
chudichen <chudichen@foxmail.com>
2020-09-19 16:03:55 +0800

    初始化

---
+++ auth-token-spring-boot-demo/src/main/java/com/chudichen/authtokenspringbootdemo/AuthTokenSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chudichen.authtokenspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTokenSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTokenSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ auth-token-spring-boot-demo/src/test/java/com/chudichen/authtokenspringbootdemo/AuthTokenSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chudichen.authtokenspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthTokenSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
