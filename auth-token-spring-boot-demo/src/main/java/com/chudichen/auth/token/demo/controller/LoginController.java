1d87f62734b5a9c23709a20a97400b178fdd9c72
chudichen <chudichen@foxmail.com>
2020-09-27 10:40:34 +0800

    添加控制器

---
+++ auth-token-spring-boot-demo/src/main/java/com/chudichen/auth/token/demo/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.chudichen.auth.token.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 登录Controller
+ *
+ * @author chudichen
+ * @since 2020-09-27
+ */
+@Controller
+@RequestMapping("/test")
+public class LoginController {
+
+
+}
---
+++ auth-token-spring-boot-demo/src/main/java/com/chudichen/auth/token/demo/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.chudichen.auth.token.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户接口
+ *
+ * @author chudichen
+ * @since 2020-09-27
+ */
+@RestController
+public class UserController {
+}
